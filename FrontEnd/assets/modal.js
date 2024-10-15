fafa06f0a70acfb3ec1835b58e9d4350cbd07afb
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2024-10-15 16:46:30 +0200

    Set up the opening and closing of the modal window
    HTML, CSS and JS code to:
    - open the modal window
    - close the modal window
    - allow the navigation to be done with the keyboard (for inclusive navigation)
    - added animations to the opening of the modal

---
+++ FrontEnd/assets/modal.js
@@ -0,0 +1,85 @@
+let modal = null;
+
+const focusableSelector = "button, a, input, textarea";
+let focusables = [];
+
+let previouslyFocusedElement = null;
+
+//Opening the modal window//
+const openModal = function (event) {
+  event.preventDefault();
+  modal = document.querySelector(event.target.getAttribute("href"));
+  focusables = Array.from(modal.querySelectorAll(focusableSelector));
+  previouslyFocusedElement = document.querySelector(":focus");
+  modal.style.display = null;
+  focusables[0].focus();
+  modal.removeAttribute("aria-hidden");
+  modal.setAttribute("aria-modal", "true");
+  modal.addEventListener("click", closeModal);
+  modal.querySelector(".js-modal-close").addEventListener("click", closeModal);
+  modal
+    .querySelector(".js-modal-stop")
+    .addEventListener("click", stopPropagation);
+};
+
+//closing the modal window//
+const closeModal = function (event) {
+  if (modal === null) return;
+  if (previouslyFocusedElement !== null) {
+    previouslyFocusedElement.focus();
+  }
+  event.preventDefault();
+  modal.style.display = "none";
+  modal.setAttribute("aria-hidden", "true");
+  modal.removeAttribute("aria-modal");
+  modal.removeEventListener("click", closeModal);
+  modal
+    .querySelector(".js-modal-close")
+    .removeEventListener("click", closeModal);
+  modal
+    .querySelector(".js-modal-stop")
+    .removeEventListener("click", stopPropagation);
+  modal = null;
+};
+
+//allowing the clicks on the modal to not close it//
+const stopPropagation = function (event) {
+  event.stopPropagation();
+};
+
+document.querySelectorAll(".js-modal").forEach((a) => {
+  a.addEventListener("click", openModal);
+});
+
+const focusInModal = function (event) {
+  event.preventDefault();
+};
+
+window.addEventListener("keydown", function (event) {
+  if (event.key === "Escape" || event.key === "Esc") {
+    closeModal(event);
+  }
+  if (event.key === "Tab" && modal !== null) {
+    focusInModal(event);
+    let index = focusables.findIndex(
+      (f) => f === modal.querySelector(":focus")
+    );
+    if (event.shiftKey === true) {
+      index--;
+    } else {
+      index++;
+    }
+    if (index >= focusables.length) {
+      index = 0;
+    }
+    if (index < 0) {
+      index = focusables.length - 1;
+    }
+    focusables[index].focus();
+  }
+});
+
+//Displaying gallery in the modal window//
+const gallery = modal.querySelector(".gallery-modal");
+
+forEach;
