0395108447c8c3fcd58d09d5a1517891e4291ec0
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2024-11-07 16:03:17 +0100

    Update apiAuthProvider.js

--- FrontEnd/assets/lib/auth/adapters/apiAuthProvider.js
+++ FrontEnd/assets/lib/auth/adapters/apiAuthProvider.js
@@ -1,1 +1,17 @@
 // TODO: Quand ça sera à connecter avec l'API, faire la même chose que fakeLogin mais en faisant des appels http vers le serveur.
+
+// async function login(email, password) {
+//     if (email != undefined && password != undefined) {
+//       data = { username: email, password: password };
+//       body = JSON.stringify(data);
+//     } else {
+//       return false;
+//     }
+
+//     response = await fetch("http://localhost:5678/api/users/login", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: body,
+//     });
