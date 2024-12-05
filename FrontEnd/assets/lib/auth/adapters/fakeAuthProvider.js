d5430c36edbec738a95072ff20ad743d66be5aea
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2024-12-05 19:28:44 +0100

    Added comments to FakeAuthProvider
    Added commentaries to fakeAuthProvider file to explain the functions within the module

--- FrontEnd/assets/lib/auth/adapters/fakeAuthProvider.js
+++ FrontEnd/assets/lib/auth/adapters/fakeAuthProvider.js
@@ -1,6 +1,10 @@
 const fakeValidEmail = "alexandre.rossruggeri@gmail.com";
 const fakeValidPassword = "password123";
 
+// Simulates a login process by checking the provided credentials //
+//(email and password) against predefined fake valid credentials //
+//(fakeValidEmail and fakeValidPassword) //
+
 async function login(email, password) {
   if (email === fakeValidEmail && password === fakeValidPassword) {
     localStorage.setItem("logged", true);
@@ -10,10 +14,14 @@ async function login(email, password) {
   }
 }
 
+//Simulates a logout process by marking the user as not logged in//
+
 async function logout() {
   localStorage.setItem("logged", false);
   return Promise.resolve();
 }
+
+// Checks whether the user is currently loggin in//
 
 function isLogged() {
   return localStorage.getItem("logged") === "true";
