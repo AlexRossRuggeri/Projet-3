220fde80814a5494a893af35b2baf96416236bc7
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-07-11 12:18:16 +0200

    Add login form handler with authentication
    Introduces a new login.js script that handles form submission, calls the authentication service, and displays error messages on failure. Redirects to the homepage upon successful login.

---
+++ FrontEnd/js/login.js
@@ -0,0 +1,21 @@
+import { login as authLogin } from './services/authService.js';
+
+const form = document.querySelector('form');
+const errorMessage = document.getElementById('errorMessage');
+
+form.addEventListener('submit', async function (event) {
+  event.preventDefault();
+  errorMessage.textContent = ' ';
+
+  const email = document.querySelector('form input#email').value;
+  const password = document.querySelector('form input#password').value;
+
+  console.log(email, password);
+
+  try {
+    await authLogin(email, password);
+    window.location.href = '../index.html';
+  } catch (error) {
+    errorMessage.textContent = error.message || 'Incorrect email or password';
+  }
+});
